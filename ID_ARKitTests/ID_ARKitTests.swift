9f58099bd39e461855be0f3bd967761c92f96a90
Caio Araujo Mariano <caioaraujomariano@MacBook-Pro-de-Caio.local>
2019-07-10 15:42:45 -0300

    feat(init): initial commit

---
+++ ID_ARKitTests/ID_ARKitTests.swift
@@ -0,0 +1,34 @@
+//
+//  ID_ARKitTests.swift
+//  ID_ARKitTests
+//
+//  Created by Caio Araujo Mariano on 10/07/19.
+//  Copyright © 2019 Caio Araujo Mariano. All rights reserved.
+//
+
+import XCTest
+@testable import ID_ARKit
+
+class ID_ARKitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
